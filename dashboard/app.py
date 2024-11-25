273687abd94e66530bce6d49040f906134c8a09f
james-0177 <james.pinkston@outlook.com>
2024-11-24 20:39:59 -0600

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,8 +1,11 @@
+
 # Import packages from pyshiny express and the python standard library
 from shiny import reactive, render
 from shiny.express import ui
 import random
+import pandas as pd
 from datetime import datetime
+from collections import deque
 from faicons import icon_svg
 
 # If working locally, add the following to requirements.txt
@@ -31,6 +34,16 @@ from faicons import icon_svg
 UPDATE_INTERVAL_SECS: int = 1
 # --------------------------------------------
 
+# Initialize a REACTIVE VALUE with a common data structure
+# The reactive value is used to store state (information)
+# Used by all the display components that show this live data.
+# This reactive value is a wrapper around a DEQUE of readings
+
+# --------------------------------------------
+DEQUE_SIZE: int = 5
+reactive_value_wrapper = reactive.value(deque(maxlen=DEQUE_SIZE))
+#---------------------------------------------
+
 # Initialize a REACTIVE CALC that our display components can call
 # to get the latest data and display it.
 # The calculation is invalidated every UPDATE_INTERVAL_SECS
@@ -53,10 +66,23 @@ def reactive_calc_combined():
     # Get a timestamp for "now" and use string format strftime() method to format it
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    latest_dictionary_entry = {"temp": temp, "timestamp": timestamp}
+    new_dictionary_entry = {"temp": temp, "timestamp": timestamp}
 
-    # Return everything we need
-    return latest_dictionary_entry
+    # get the deque and append the new entry
+    reactive_value_wrapper.get().append(new_dictionary_entry)
+
+    # Get a snapshot of the current deque for any further processing
+    deque_snapshot = reactive_value_wrapper.get()
+
+    # For Display: Convert deque to DataFrame for display
+    df = pd.DataFrame(deque_snapshot)
+
+    # For Display: Get the latest dictionary entry
+    latest_dictionary_entry = new_dictionary_entry
+
+    # Return a tuple with everything we need
+    # Every time we call this function, we'll get all these values
+    return deque_snapshot, df, latest_dictionary_entry
 
 # ------------------------------------------------
 # Define the Shiny UI Page layout - Page Options
@@ -66,7 +92,7 @@ def reactive_calc_combined():
 # Set title to a string in quotes that will appear at the top
 # Set fillable to True to use the whole page width for the UI
 
-ui.page_opts(title="PyShiny Express: Live Data (Basic)", fillable=True)
+ui.page_opts(title="PyShiny Express: Live Data with Value Card (Font Awesome Icon + 3 strings)", fillable=True)
 
 # ------------------------------------------------
 # Define the Shiny UI Page layout - Sidebar
@@ -87,10 +113,14 @@ with ui.sidebar(open="open"):
     ui.h6("Links")
     ui.a(
         "GitHub Source",
-        href="https://github.com/james-0177/cintel-05-cintel-basic/",
+        href="https://github.com/james-0177/cintel-05-cintel/",
         target="_blank",
     )
-
+    ui.a(
+        "GitHub App",
+        href="https://github.com/james-0177/cintel-05-cintel/blob/main/dashboard/app.py",
+        target="_blank",
+    )
     ui.a("PyShiny", href="https://shiny.posit.co/py/", target="_blank",)
     
 
@@ -98,28 +128,30 @@ with ui.sidebar(open="open"):
 # In Shiny Express, everything not in the sidebar is in the main panel
 #---------------------------------------------------------------------
 
+with ui.layout_columns():
+    with ui.value_box(
+        showcase=icon_svg("sun"),
+        theme="bg-gradient-blue-purple",
+    ):
 
-ui.h2("Current Temperature")
+        "Current Temperature"
 
-@render.text
-def display_temp():
-    """Get the latest reading and return a temperature string"""
-    latest_dictionary_entry = reactive_calc_combined()
-    return f"{latest_dictionary_entry['temp']} C"
+        @render.text
+        def display_temp():
+            """Get the latest reading and return a temperature string"""
+            deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+            return f"{latest_dictionary_entry['temp']} C"
 
-ui.p("warmer than usual")
+        "warmer than usual"
 
-icon_svg("snowflake")
+    with ui.card(full_screen=True):
+        ui.card_header("Current Date and Time")
 
-ui.hr()
-
-ui.h2("Current Date and Time")
-
-@render.text
-def display_time():
-    """Get the latest reading and return a timestamp string"""
-    latest_dictionary_entry = reactive_calc_combined()
-    return f"{latest_dictionary_entry['timestamp']}"
+        @render.text
+        def display_time():
+            """Get the latest reading and return a timestamp string"""
+            deque_snapshot, df, latest_dictionary_entry = reactive_calc_combined()
+            return f"{latest_dictionary_entry['timestamp']}"
 
 with ui.layout_columns():
     with ui.card():
